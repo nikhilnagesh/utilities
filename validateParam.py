ad55cda57d3b1eead49f770fb15ca5054dab02ff
Nikhil Nagesh <10531163+nikhilnagesh@users.noreply.github.com>
2020-09-10 11:01:48 +0530

    Update and rename test_validateParam.py to validateParam.py

--- validateParam.py
+++ validateParam.py
@@ -4,7 +4,7 @@
 #  Author: Nikhil Nagesh
 #  Email: Nikhil.Nagesh@evry.com
 #  Created Date:  08/26/2020
-#  Last Modified Date: 08/26/2020
+#  Last Modified Date: 09/10/2020
 #  Executing method: python validateParam.py PROVIDER w_127_B4T_101_LOAD_ACTIVITY_REC_STG 127_B4T_PROVIDER
 #                    python validateParam.py RESOURCE wf_SET_445_RSC_V1 SET_445_RSC_V1
 #                    python validateParam.py PRESTAGE wf_XKTX3TNRX_V1_ClmsPro_SRC_2_PSTG XKTX3TNRX_V1_ClmsPro_src2pstg
@@ -116,7 +116,7 @@ elif len(sys.argv) > 1:
 #######################################################################################################################################
 #  Methods section
 #######################################################################################################################################
-def test_func_Proc(pProc):
+def func_Proc(pProc):
     return {
         'provider': 'tst_provider',
         'providerloads': 'tst_provider',
@@ -136,7 +136,7 @@ def test_func_Proc(pProc):
     }[pProc]
 
 
-def test_func_ValidateParam(arg_ParamFile):
+def func_ValidateParam(arg_ParamFile):
     global vlErroCntr
     for iLnNum, iLines in enumerate(arg_ParamFile, 1):
         iLine = iLines.strip()
@@ -203,7 +203,7 @@ def test_func_ValidateParam(arg_ParamFile):
                     vlErroCntr += 1
 
 
-def test_func_WorkflowPropCheck(arg_ParsedXml):
+def func_WorkflowPropCheck(arg_ParsedXml):
     global vlErroCntr
     for node in arg_ParsedXml.iter('WORKFLOW'):
         for snode in node:
@@ -230,7 +230,7 @@ def test_func_WorkflowPropCheck(arg_ParsedXml):
 #######################################################################################################################################
 
 
-def test_main():
+def main():
     logging.info("Run Date/Time: {}".format(vlDateTime))
     logging.info("User Name: {}".format(vlUserName))
     logging.info("Process Name: {}".format(pProc))
@@ -356,4 +356,4 @@ def test_main():
 
 
 if __name__ == "__main__":
-    test_main()
+    main()
