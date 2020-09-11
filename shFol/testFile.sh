c139b947d8dafe1409f51edc96ae8fd54960d605
Nikhil Nagesh <10531163+nikhilnagesh@users.noreply.github.com>
2020-09-11 15:59:40 +0530

    Update testFile.sh

--- shFol/testFile.sh
+++ shFol/testFile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GITHUB_WORKSPACE=$1
+GITHUB_WORKSPACE=`pwd`
 file_name=${GITHUB_WORKSPACE}/hms_deployment_manager.txt
 
-content=$(
+content1=$(
   sed '
     s/[[:space:]]\{1,\}/ /g; # turn sequences of spacing characters into one SPC
     s/[^[:print:]]//g; # remove non-printable characters
@@ -11,9 +11,37 @@ content=$(
     q; # quit after first line' < "$file_name"
 )
 
-if [ "$content" = 'DO NOT REMOVE FIRST THREE LINES' ]; then
-  echo 'Test Success'
+content2=$(
+  sed -n '2{p;q}' < "$file_name"
+)
+
+echo $content2
+
+content3=$(
+  sed -n '3{p;q}' < "$file_name"
+)
+
+echo $content3
+
+
+
+if [ "$content1" = 'DO NOT REMOVE FIRST THREE LINES' ]; then
+  echo 'Test Success - 1st line is proper'
 else
-echo 'Test Failed - hms_deployment_manager.txt is not correct'
-exit 1
+echo 'Test Failed - 1st line is not proper in hms_deployment_manager.txt'
+exit 0
+fi
+
+if [ "$content2" = 'example: <CHANGE_NUMBER>:<WORKFLOW_FILE.xml>:<CONTROL_FILE.xml>:<integration_service>' ]; then
+  echo 'Test Success - 2nd line is proper'
+else
+echo 'Test Failed - 2nd line is not proper in hms_deployment_manager.txt'
+exit 0
+fi
+
+if [ "$content3" = '------------------------------------------------------------------------------------------------' ]; then
+  echo 'Test Success - 3rd line is proper'
+else
+echo 'Test Failed - 3rd line is not proper in hms_deployment_manager.txt'
+exit 0
 fi
